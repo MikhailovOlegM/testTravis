4b290978bbec3a917052415199d3eb86275f5ebb
Oleg Mikhailov <Oleg Mikhailov>
2017-01-09 20:30:32 +0200

    test

---
+++ src/main/java/com/hillel/tastks/SumOfNumbers.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by mikha on 09.01.2017.
+ */
+public class SumOfNumbers {
+}
---
+++ src/test/java/com/hillel/tasks/testSum.java
@@ -0,0 +1,7 @@
+package com.hillel.tasks;
+
+/**
+ * Created by mikha on 09.01.2017.
+ */
+public class testSum {
+}
